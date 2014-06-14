767506e8b68c2f864a9f330554c08dd4792085d5
redhatlinux10 <bruce.oy@gmail.com>
2014-06-15 00:02:08 +0800

    -

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 rm -f plasmoid-drop2YaST.plasmoid 
-zip  -r plasmoid-drop2YaST.plasmoid . -x \*.git\* build.sh plasmoid-drop2YaST.plasmoid
+zip  -r plasmoid-drop2YaST.plasmoid . -x \*.git\* build.sh plasmoid-drop2YaST.plasmoid snapshot.png
